cb3eaaace1d0879359a7b3eb274199873572f16d
Javier Ros <jros@emuasa.es>
2016-01-15 13:26:09 +0100

    Fix texts to pass with the new prefix default

--- test/expected/excludeExp/foo-mx.d.ts
+++ test/expected/excludeExp/foo-mx.d.ts
@@ -3,15 +3,15 @@
 //   ../../src/typings/external.d.ts
 
 declare module 'foo-mx' {
-    import exp = require('__foo-mx/lib/exported-sub');
+    import exp = require('foo-mx/lib/exported-sub');
     import mod1 = require('external1');
-    export import Foo = require('__foo-mx/Foo');
+    export import Foo = require('foo-mx/Foo');
     export function run(foo?: Foo): Foo;
     export function flep(): exp.ExternalContainer;
     export function bar(): mod1.SomeType;
 }
 
-declare module '__foo-mx/Foo' {
+declare module 'foo-mx/Foo' {
     class Foo {
             foo: string;
             constructor(secret?: string);
--- test/expected/remove/foo-mx.d.ts
+++ test/expected/remove/foo-mx.d.ts
@@ -3,16 +3,16 @@
 //   ../../src/typings/external.d.ts
 
 declare module 'foo-mx' {
-    import exp = require('__foo-mx/lib/exported-sub');
+    import exp = require('foo-mx/lib/exported-sub');
     import mod1 = require('external1');
-    export import Foo = require('__foo-mx/Foo');
+    export import Foo = require('foo-mx/Foo');
     export function run(foo?: Foo): Foo;
     export function flep(): exp.ExternalContainer;
     export function bar(): mod1.SomeType;
 }
 
-declare module '__foo-mx/lib/exported-sub' {
-    import Foo = require('__foo-mx/Foo');
+declare module 'foo-mx/lib/exported-sub' {
+    import Foo = require('foo-mx/Foo');
     import mod2 = require('external2');
     export class ExternalContainer {
         something: mod2.AnotherType;
@@ -21,7 +21,7 @@ declare module '__foo-mx/lib/exported-sub' {
     export function bazz(value: string, option?: boolean): string;
 }
 
-declare module '__foo-mx/Foo' {
+declare module 'foo-mx/Foo' {
     class Foo {
             foo: string;
             constructor(secret?: string);
